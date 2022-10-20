2381aef8c48dabbc0ee195016d690b4cb95d2a30
Agilesh-s <agileshsivakumar.s@gmail.com>
2022-10-20 22:42:28 +0530

    View Employee

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-employee',
@@ -7,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myApi:ApiService) { }
+
+  employee_id = ""
+  employeee_name = ""
+  designation = ""
+  salary = ""
+  address = ""
+
+  readValues = ()=>{
+    let data = {
+      "employee_id":this.employee_id,
+      "employee_name":this.employeee_name,
+      "designation":this.designation,
+      "salary":this.salary,
+      "address":this.address
+    }
+    console.log(data)
+    this.myApi.addEmployee(data).subscribe(
+      (response)=>{
+        alert("ADDED SUCCESSFULLY")
+      }
+    )
+    this.employee_id = ""
+    this.employeee_name = ""
+    this.designation = ""
+    this.salary = ""
+    this.address = ""
+  }
 
   ngOnInit(): void {
   }
